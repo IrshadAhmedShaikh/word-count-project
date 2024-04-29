10dbf0dd6019db22898787b753314c14780169e3
Irshad Ahmed <160173156+IrshadAhmedShaikh@users.noreply.github.com>
2024-04-29 17:55:28 +0500

    This is a word count project

---
+++ index.ts
@@ -0,0 +1,10 @@
+import inquirer from "inquirer";
+
+const answers=await inquirer.prompt([{
+  name:"sentance",
+  type:"input",
+  message:"Enter your sentance to count words:"
+}]);
+const words=answers.sentance.trim().split(" ")
+console.log(words)
+console.log(`your count words is:${words.length}`);
